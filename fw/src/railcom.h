049274b0450f820ccbe473d82d4f9e3e9846a801
Jan Horacek <me@apophis.cz>
2024-03-16 10:51:06 +0100

    Add railcom_ll api.

--- fw/src/railcom.h
+++ fw/src/railcom.h
@@ -1,8 +1,35 @@
-/* RailCom USART reading */
+/* RailCom USART low-level reading
+ * Output: bytes parsed by RailCom
+ */
 
 #pragma once
 
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define RC_BAUDRATE 250000
+#define RC_CH1_SIZE 2 // bytes
+#define RC_CH2_SIZE 5 // bytes
+
+/* Higher layers poll rlccdata, when ready_to_parse is true, higher layer can
+ * read the data. Once reading is finished, higher layer sets ready_to_parse = false,
+ * which is an indication for low-level layer that next reading could be performed.
+ */
+
+typedef struct {
+    struct {
+        uint8_t buf[RC_CH1_SIZE];
+        size_t size;
+    } ch1;
+    struct {
+        uint8_t buf[RC_CH2_SIZE];
+        size_t size;
+    } ch2;
+    bool ready_to_parse;
+} RCLLData;
+
+extern volatile RCLLData rclldata;
 
 void railcom_init(void);
 void railcom_deinit(void);
