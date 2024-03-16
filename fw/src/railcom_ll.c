aeee80dc64134b763d511987d5abdf4b7bc834af
Jan Horacek <me@apophis.cz>
2024-03-16 13:00:10 +0100

    RC LL: make usart2 work.

--- fw/src/railcom_ll.c
+++ fw/src/railcom_ll.c
@@ -76,6 +76,31 @@ void _rc1_init(void) {
 }
 
 void _rc2_init(void) {
+    __HAL_RCC_USART2_CLK_ENABLE();
+
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate = RC_BAUDRATE;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Mode = UART_MODE_RX;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    assert_param(HAL_UART_Init(&huart2) == HAL_OK);
+
+    gpio_pin_init(pin_rc2, GPIO_MODE_INPUT, GPIO_NOPULL, GPIO_SPEED_FREQ_HIGH, false);
+
+    /* USART2 interrupt Init */
+    HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(USART2_IRQn);
+
+    assert_param(HAL_UART_RegisterCallback(&huart2, HAL_UART_RX_COMPLETE_CB_ID, _rcll_uart_rx_complete) == HAL_OK);
+
+    // Disable receiver till cutout
+    CLEAR_BIT(USART2->CR1, USART_CR1_RE);
+
+    // Start first reading
+    assert_param(HAL_UART_Receive_IT(&huart2, (uint8_t*)&uart2_input_byte, 1) == HAL_OK);
 }
 
 void _tim4_init(void) {
@@ -102,6 +127,7 @@ void railcom_ll_deinit(void) {
 
     __HAL_RCC_USART2_CLK_DISABLE();
     gpio_pin_deinit(pin_rc2);
+    HAL_NVIC_DisableIRQ(USART2_IRQn);
 }
 
 /* Cutout --------------------------------------------------------------------*/
@@ -137,24 +163,25 @@ void cutout_start(void) {
     }
 
     SET_BIT(USART1->CR1, USART_CR1_RE);
+    SET_BIT(USART2->CR1, USART_CR1_RE);
 }
 
 void cutout_end(void) {
     assert_param(HAL_TIM_Base_Stop(&htim4) == HAL_OK);
 
-    for (size_t i = 0; i < RC_UARTS_COUNT; i++) {
-        if ((_rc_receiving) && ((rclldata[i].ch1.size > 0) || (rclldata[i].ch2.size > 0))) {
+    for (size_t i = 0; i < RC_UARTS_COUNT; i++)
+        if ((_rc_receiving) && ((rclldata[i].ch1.size > 0) || (rclldata[i].ch2.size > 0)))
             rclldata[i].ready_to_parse = true;
-        }
-    }
     _rc_receiving = false;
     CLEAR_BIT(USART1->CR1, USART_CR1_RE);
+    CLEAR_BIT(USART2->CR1, USART_CR1_RE);
 }
 
 void cutout_timeout(void) {
     assert_param(HAL_TIM_Base_Stop(&htim4) == HAL_OK);
     _rc_receiving = false;
     CLEAR_BIT(USART1->CR1, USART_CR1_RE);
+    CLEAR_BIT(USART2->CR1, USART_CR1_RE);
 }
 
 uint16_t cutout_length_us(void) {
@@ -167,6 +194,10 @@ void USART1_IRQHandler(void) {
     HAL_UART_IRQHandler(&huart1);
 }
 
+void USART2_IRQHandler(void) {
+    HAL_UART_IRQHandler(&huart2);
+}
+
 void _rcll_uart_rx_complete(UART_HandleTypeDef *huart) {
     if (huart == &huart1)
         assert_param(HAL_UART_Receive_IT(&huart1, (uint8_t*)&uart1_input_byte, 1) == HAL_OK);
@@ -177,18 +208,13 @@ void _rcll_uart_rx_complete(UART_HandleTypeDef *huart) {
         return;
 
     volatile RCLLData* uartdata = (huart == &huart1) ? &rclldata[0] : &rclldata[1];
+    const uint8_t received = (huart == &huart1) ? uart1_input_byte : uart2_input_byte;
 
-    if (cutout_length_us() < RC_CH1_MAX_LEN) {
-        if (uartdata->ch1.size < RC_CH1_SIZE) {
-            gpio_pin_toggle(pin_debug_1);
-            uartdata->ch1.size++;
-            uartdata->ch1.buf[uartdata->ch1.size] = uart1_input_byte;
-        }
-    } else {
-        if (uartdata->ch2.size < RC_CH2_SIZE) {
-            gpio_pin_toggle(pin_debug_2);
-            uartdata->ch2.size++;
-            uartdata->ch2.buf[uartdata->ch2.size] = uart2_input_byte;
-        }
+    if ((cutout_length_us() < RC_CH1_MAX_LEN) && (uartdata->ch1.size < RC_CH1_SIZE)) {
+        uartdata->ch1.size++;
+        uartdata->ch1.buf[uartdata->ch1.size] = received;
+    } else if (uartdata->ch2.size < RC_CH2_SIZE) {
+        uartdata->ch2.size++;
+        uartdata->ch2.buf[uartdata->ch2.size] = received;
     }
 }
