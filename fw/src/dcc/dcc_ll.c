0c72f0b4ee4307d4f334e33458ed574aa6bf99c5
Jan Horacek <me@apophis.cz>
2024-03-21 15:10:02 +0100

    Add dcc parsing template.

---
+++ fw/src/dcc/dcc_ll.c
@@ -0,0 +1,10 @@
+/* DCC packets parsing low-level implementation */
+
+#include "dcc_ll.h"
+
+/* Local variables -----------------------------------------------------------*/
+
+/* Private prototypes --------------------------------------------------------*/
+
+/* Implementation ------------------------------------------------------------*/
+
---
+++ fw/src/dcc/dcc_ll.h
@@ -0,0 +1,9 @@
+/* DCC low-level signal decoding
+ * Output: DCC packet
+ */
+
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
